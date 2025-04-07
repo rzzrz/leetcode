6b674c186557e7416f299740eed11762f626f826
2197947404@qq.com <rzz-rz@rzzrz.>
2025-04-07 14:24:45 +0800

    leetcode-70'

--- leet.cpp
+++ leet.cpp
@@ -6,27 +6,30 @@
 
 using namespace std;
 
+/*
+    首先爬楼梯的话 将子问题拆分成一个一个小问题
+    首先爬上第一节楼梯的话就之后选择一步他上去
+    到了第二节的话就是step一次从第一个阶梯他上来的 dp[0] = 1
+    或者一次迈了两个台阶那就是由  dp[1] = dp[1 - 1] 
+    第三个台阶的话就是从第一个台阶迈上两格直接上来的或者从第二个台阶
+    dp[2] = dp[2 - 1] + dp[2 - 2]
+*/
 class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        using ll = long long;
-        ll res =0;
-        ll left = 0;
-        ll right = height.size() - 1;
-        while(left < right){
-            res = max(res,(right - left) *min(height[left],height[right]));
-            if(height[left] < height[right])
-                left++;
-            else
-                right--;
+    public:
+        int climbStairs(int n) {
+            vector<int> dp(n + 1,0);
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i = 2;i < n;i++){                
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
         }
-        return res;
-    }
-};
+    };
 
 int main(){
     vector<int> heigt = {1,8,6,2,5,4,8,3,7};
     Solution s;
-    s.maxArea(heigt);
+    
     return 0;
 }
