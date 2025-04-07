39229f3f7c74a3d8a4490bab94b39d341ba23957
2197947404@qq.com <rzz-rz@rzzrz.>
2025-04-07 14:03:07 +0800

    leetcode-179

---
+++ CMakeFiles/leet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leet.dir/leet.cpp.o"
+  "CMakeFiles/leet.dir/leet.cpp.o.d"
+  "leet"
+  "leet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- leet.cpp
+++ leet.cpp
@@ -2,40 +2,31 @@
 #include<string>
 #include<algorithm>
 #include<iostream>
+#include<stack>
+
 using namespace std;
 
 class Solution {
-    public:
-        vector<int> partitionLabels(string s) {
-            // 将每个字符最后出现的位置记录下来，
-            // 之后遍历看当前元素是不是最后有次出现，
-            // 并且可以维护一个哈希表记录还没有因为
-            // 最后一次出现消除的
-            int last[26]{0};
-
-            int n = s.size();
-            for(int i = 0; i < n; ++i)
-                last[s[i] - 'a'] = i;
-
-            vector<int> res;
-            int max_len = 0;
-            int begin = 0;
-            for(int i = 0;i < n; ++i){
-                max_len = max(max_len , last[s[i] - 'a']);
-                if(i == max_len){
-                    res.push_back(i + 1 - begin);
-                    begin = i + 1;
-                }
-                    
-            }
-            return res;
+public:
+    int maxArea(vector<int>& height) {
+        using ll = long long;
+        ll res =0;
+        ll left = 0;
+        ll right = height.size() - 1;
+        while(left < right){
+            res = max(res,(right - left) *min(height[left],height[right]));
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
         }
-    };
+        return res;
+    }
+};
 
 int main(){
-    string str;
-    cin >> str;
+    vector<int> heigt = {1,8,6,2,5,4,8,3,7};
     Solution s;
-    s.partitionLabels(str);
+    s.maxArea(heigt);
     return 0;
 }
