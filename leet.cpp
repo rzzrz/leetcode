d7cc7b09b9c14c36ba954aa5eae3b07e1e34ecb3
2197947404@qq.com <rzz-rz@rzzrz.>
2025-04-08 11:39:39 +0800

    leetcode-213

--- leet.cpp
+++ leet.cpp
@@ -8,40 +8,31 @@ using namespace std;
 
 class Solution {
 public:
-    int countHousePlacements(int n) {
-        // 根据题目要求我们发现街道两侧的房子放置情况不会不想影响，
-        // 且满足乘法原理随意我们只需要计算一侧的可能的次数以后
-        // 根据乘法原理乘以自己就可以了
-        // 我们将问题简化就变成了打家劫舍问题
-        // 我们定义dp[i]为到当前i地块有多少种组合方式
-        // 根据递归的界限发现取值范围是 -2 -1 0 ... n-1 共n + 2元素
-        using ll = long long;
-        vector<ll> dp(n + 2,0);
-        const ll MOD = 1e9 + 7;
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(2,vector<int>(n+2,0));
         //
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i = 2; i<= n+1; i++){
-            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
-            // dp[i - 1] 和 dp[i - 2]表示了
-            // 当前i地痞没有被选择所以我们选择加上他上一个地皮的组合数目
-            // 和当前I地皮被选择了这时候就隐含了他的上一块地皮没有被选择
-            // 所以我们这个语句就已经表示了当前地皮有没有被选择的含义
+        for(int i = 2; i<=n+1;i++){
+            dp[0][i] = max(dp[0][i - 1], nums[i-2] + dp[0][i - 2]);
         }
-        return (dp[n] * dp[n]) % MOD;
+        for(int i = 3; i<=n+2;i++){
+            dp[1][i] = max(dp[1][i - 1], nums[i-2] + dp[1][i - 2]);
+        }
+        return max(dp[0][n+1],dp[1][n+2]);
     }
+
 private:
-    // 对于选择当前地痞建房子的话那么就意味着 
-    // int dfs(int i){
-    //     if(i<0)
-    //         return 1;
-    //     return dfs(i - 1) + dfs(i - 2);
-    // }
+    // int dfs(int low,int n,vector<int>& nums){
+    //     if(n<low)
+    //         return 0;
+    //     return max(dfs(low,n-1,nums),(nums[n] + dfs(low,n-2,nums)));
+    // } 
 };
 
 int main(){
+    vector<int> nums = {1,2,3,1};
     Solution s;
-    std::cout<<s.countHousePlacements(3);
+    std::cout<<s.rob(nums);
 
     return 0;
 }
