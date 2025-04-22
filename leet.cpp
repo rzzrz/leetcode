c830aa9b52d2f99b688f7ae6d0ac3822ff3ffe73
2197947404@qq.com <rzz-rz@rzzrz.>
2025-04-22 08:42:16 +0800

    leetcode-1091 , 该用ninja生成器

--- leet.cpp
+++ leet.cpp
@@ -6,53 +6,49 @@ using namespace std;
 
 class Solution {
 public:
-  int nearestExit(vector<vector<char>> &maze, vector<int> &entrance) {
-    // 创建队列模拟bfs
-    std::queue<std::pair<int, int>> q;
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+      // 使用bfs进行便利 用一个数组维护当前传播距离
+      int n = grid.size();
+      std::vector<std::vector<int>> vec(n,vector<int>(n,1));
+      std::queue<pair<int, int>> q;
 
-    int n = maze.size();
-    int m = maze[0].size();
+      // 方向数组
+      int dx[8] = {-1, -1, -1, 0, 0, +1, +1, +1};
+      int dy[8] = {-1, 0, +1, -1, +1, -1, 0, +1};
 
-    std::vector<std::vector<int>> dis(n,vector<int>(m,0));
+      if (grid[0][0])
+        return -1;
 
-    // 遵从上下左右
-    int x[4] = {-1, +1, 0, 0};
-    int y[4] = {0, 0, -1, +1};
+      q.push({0, 0});
+      vec[0][0] = 1;
+      
+      while (!q.empty()) {
+        pair<int, int> now_pos = q.front();
+        q.pop();
 
-    maze[entrance[0]][entrance[1]] = '+';
-    q.push({entrance[0], entrance[1]});
+        if (now_pos.first == n-1 && now_pos.second == n-1)
+          return vec[now_pos.first][now_pos.second];
 
-    bool first_enter = false;
+        for (int i = 0; i < 8; i++) {
+          int x = now_pos.first + dx[i];
+          int y = now_pos.second + dy[i];
 
-    while (!q.empty()) {
-      std::pair<int, int> pos = q.front();
-      q.pop();
-
-      if ((pos.first == 0 || pos.first == n - 1 || pos.second == 0 ||
-          pos.second == m - 1) && first_enter)
-        return dis[pos.first][pos.second];
-
-      for (int i = 0; i < 4; i++) {
-        int dx = pos.first + x[i];
-        int dy = pos.second + y[i];
-        if (dx >= 0 && dy >= 0 && dx < n && dy < m && maze[dx][dy] == '.') {
-          maze[dx][dy] = '+';
-          dis[dx][dy] = dis[pos.first][pos.second] + 1;
-          q.push({dx,dy});
+          if (x >= 0 && x < n && y >= 0 && y < n && grid[x][y] == 0) {
+            grid[x][y] = 1;
+            q.push({x, y});
+            vec[x][y] = vec[now_pos.first][now_pos.second] + 1;
+          }
         }
       }
-      first_enter = true;
+      return -1;
     }
-    return -1;
-  }
-  
 };
 
 int main() {
   vector<int> nums = {1,1,1,1,1};
 
   Solution s;
-  std::cout << s.findTargetSumWays(nums,3);
-  std::cout << "endl" << endl;
+  //std::cout << s.findTargetSumWays(nums,3);
+  //std::cout << "endl" << endl;
   return 0;
 }
